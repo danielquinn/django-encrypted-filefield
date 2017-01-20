c57c57bb9a33f839300216a238b32a54bad1997d
Bart Heesink <bheesink@jouwomgeving.nl>
2017-01-20 10:50:07 +0100

    Add option to define DEFF constants in settings

--- django_encrypted_filefield/constants.py
+++ django_encrypted_filefield/constants.py
@@ -1,5 +1,7 @@
 import os
 
+from django.conf import settings
+
 
 def get_bytes(name):
     r = os.getenv("DEFF_{}".format(name))
@@ -7,7 +9,6 @@ def get_bytes(name):
         return bytes(r.encode("utf-8"))
     return None
 
-
-SALT = get_bytes("SALT")
-PASSWORD = get_bytes("PASSWORD")
-FETCH_URL_NAME = os.getenv("DEFF_FETCH_URL_NAME")
+SALT = get_bytes("SALT") or getattr(settings, 'DEFF_SALT', None)
+PASSWORD = get_bytes("PASSWORD") or getattr(settings, 'DEFF_PASSWORD', None)
+FETCH_URL_NAME = os.getenv("DEFF_FETCH_URL_NAME") or getattr(settings, 'DEFF_FETCH_URL_NAME', None)
